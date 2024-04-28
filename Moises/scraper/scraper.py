4e2422604587f4b6d5293d540f60c9b46261fa13
moisesrobles-04 <moises.robles@upr.edu>
2024-04-27 22:27:17 -0400

    Update scraper to travel pubmed. Now can interact with other pages

--- Moises/scraper/scraper.py
+++ Moises/scraper/scraper.py
@@ -14,113 +14,95 @@ nih = 'https://www.ncbi.nlm.nih.gov'
 term = ['covid vaccine', 'covid treatment', 'covid symptoms', 'covid sickness']
 
 
-def pubmed_scrapper(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--start-maximized')
-    with webdriver.Chrome(options= options) as driver:
-        driver.get(url)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="abstract-a.adk.b.ad"]/div[2]')))
-        title = driver.find_element(By.XPATH, '//*[@id="ui-ncbiinpagenav-1"]/div[1]/h1').text
-        authors = driver.find_element(By.XPATH, '//*[@id="ui-ncbiinpagenav-1"]/div[1]/div[2]').text
-        doi = driver.find_element(By.XPATH, '//*[@id="ui-ncbiinpagenav-1"]/div[1]/div[1]/div[1]/div/div[2]/span[2]/a').text
-        context = driver.find_element(By.XPATH, '//*[@id="abstract-a.adk.b.ad"]/div[2]').text
-        fp = False
+def process_links(links):
+    output = []
 
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/main/aside/div/section[2]/ul/li[1]/button')))
+    # Access every link and extract content
+    for element in links:
+        title = element['title']
+        page = requests.get(element['link'], headers={'User-Agent': 'Mozilla/5.0'})
+        time.sleep(0.25)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-        # Press citation button
-        driver.find_element(By.XPATH, '/html/body/main/aside/div/section[2]/ul/li[1]/button').click()
-        time.sleep(2)
+        xml = soup.contents
+        try:
+            list_temp = [title, element['link'], xml]
+            output.append(list_temp)
+            print(f'The link: {element["link"]} was processed')
 
-        reference = driver.find_element(By.XPATH, '//*[@id="ui-ncbiexternallink-3"]/div[4]/div/div[2]/div[1]').text
-        result = [title, context, doi, reference, fp, authors]
+        except:
+            print(f'The link: {element["link"]} failed!')
+            continue
 
-        print(result)
-        create_csv("text", result)
+    return output
 
 
 def soup_pubmed_scrapper(term):
     url = f"https://ncbi.nlm.nih.gov/pmc/?term={term}"
-    page = requests.get(url)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    # result = [title, context, doi, reference, fp, authors]
-
-    # Get all links that are on the site that are not copies or pdfs
-    result = soup.findAll('a',  href=True)
+    # Browser to use selenium to click buttons
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--start-maximized')
     links = []
-    for element in result:
-        if element['href'].__contains__('/articles')\
-                and not element['href'].__contains__('pdf') and not element['href'].__contains__('/classic'):
-            l_paper = {'title': element.text, 'link': nih + element['href']}
-            links.append(l_paper)
-    print(links)
+    with webdriver.Chrome() as driver:
+        driver.get(url)
 
-    temp = []
-    print('='*30)
-    print("Get data from site")
-    print('=' * 30)
+        # Iterate to X amount of pages
+        for i in range(2):
+            count = 0
+            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="maincontent"]')))
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    for i in links:
-        if i['link'].__contains__('pdf') or i['link'].__contains__('classic'):
-            continue
+            # Get all links that are on the site that are not copies or pdfs
+            result = soup.findAll('a', href=True)
+            for element in result:
+                # Prevent unverified papers from being added
+                if count == 20:
+                    break
 
-        title = i['title']
-        page = requests.get(i['link'], headers={'User-Agent':'Mozilla/5.0'})
-        soup = BeautifulSoup(page.content, 'html.parser')
-        abstract = soup.find(attrs={'id':re.compile("abstract")})
-        if not abstract:
-            abstract = soup.find(attrs={'id':re.compile("abs")})
+                if element['href'].__contains__('/articles') \
+                        and not element['href'].__contains__('pdf') and not element['href'].__contains__('classic') \
+                        and element.text != '\n\n':
+                    l_paper = {'title': element.text, 'link': nih + element['href']}
+                    links.append(l_paper)
+                    count += 1
 
-        if abstract:
-            abstract = abstract.text
+            driver.find_element(By.XPATH, '//*[@class="active page_link next"]').click()
+            time.sleep(1)
 
-        content = soup.contents
-        text = soup.text.replace("\n", "")
+    # Print progress
+    log(2)
 
-    # Useful for getting sections
-        # text = soup.findAll('div', id=re.compile("sec-"))
-        # extract = ''
-        # for element in text:
-        #     extract += element.text
-        # print(text)
+    data = process_links(links)
 
-        print(i['link'])
-        list_temp = [title, i['link'], abstract, content, text]
-        # print(list_temp)
-        temp.append(list_temp)
-
-    create_csv(term, temp)
+    create_csv(term, data)
 
 
 def create_csv(term, data):
     csv_file = f'research_{term}'
-    with open(f'data/{csv_file}.csv', 'w', newline='', encoding="utf-8") as file:
+    with open(f'data/new_{csv_file}.csv', 'w', newline='', encoding="utf-8") as file:
         writer = csv.writer(file)
-        header = ['Title', 'Link', 'Abstract', 'XML', 'Text']
+        header = ['Title', 'Link', 'XML']
         writer.writerow(header)
         for i in data:
             writer.writerow(i)
         print(f'Successfully created file')
 
 
-# def create_csv(data):
-#     with open('data/research_test.csv', 'w', newline='', encoding="utf-8") as file:
-#         writer = csv.writer(file)
-#         header = ['Title', 'Context', 'doi', 'reference', 'Full Paper', 'Authors']
-#         writer.writerow(header)
-#         writer.writerow(data)
+def log(num):
+    print('=' * 40)
+    if num == 1:
+        print(' ' * 11, "Starting scraper")
+    elif num == 2:
+        print(' ' * 5, "Data scrapped successfully")
+    elif num == 3:
+        print(' ' * 10, "Finished Execution")
+    print('=' * 40, '\n\n')
 
 
 if __name__ == "__main__":
-    # link = "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9305720/"
-    # link = f"https://ncbi.nlm.nih.gov/pmc/?term={term}"
-    print('=' * 40)
-    print(' ' * 11, "Starting scraper")
-    print('=' * 40)
+    log(1)
     for i in term:
         soup_pubmed_scrapper(i)
-    print('=' * 40)
-    print(' ' * 10, "Finished Execution")
-    print('=' * 40)
+        break
+    log(3)
