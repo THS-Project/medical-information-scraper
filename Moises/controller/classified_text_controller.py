a4d1600c855dca0e67ac8e246ec14455fbde6ca4
moisesrobles-04 <moises.robles@upr.edu>
2024-09-05 14:24:25 -0400

    Testing classifier endpoint

--- Moises/controller/classified_text_controller.py
+++ Moises/controller/classified_text_controller.py
@@ -1,15 +1,13 @@
 from flask import jsonify
 from Moises.model.classified import ClassifiedDAO
-
+from Moises.classifier.classification import ModelPredict
 
 class ClassifiedController:
 
     @staticmethod
     def build_texts_dict(elements):
         result = {'text_id': elements[0],
-                  't_context': elements[1],
-                  'health': elements[2],
-                  'misinformation': elements[3]
+                  't_context': elements[1]
                   }
         return result
 
@@ -22,13 +20,22 @@ class ClassifiedController:
     def getAllTexts(self):
         dao = ClassifiedDAO()
         author_list = dao.getAllTexts()
-        author = [self.build_texts_dict(row) for row in author_list]
-        return jsonify(author), 200
+        result = [self.build_texts_dict(row) for row in author_list]
+        return jsonify(result), 200
 
     def getTextsById(self, text_id):
         dao = ClassifiedDAO()
         classified_text = dao.getTextById(text_id)
         if not classified_text:
             return jsonify(f"Text with id '{text_id}' was not found"), 404
-        author = self.build_texts_dict(classified_text)
-        return jsonify(author), 200
+        health = sequence_classification(classified_text[1])
+
+        if health == 'Related':
+
+
+        result = self.build_texts_dict(classified_text)
+        return jsonify(result), 200
+
+
+def sequence_classification(text: str):
+    return ModelPredict(mname=, classtype=, num=, datatype=).sequence_classification()
